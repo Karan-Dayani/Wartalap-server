1165ce472711b1487a176462313a40bb56dc9d7b
Karan <karandayani39@gmail.com>
2024-10-15 15:38:14 +0530

    created users and avatar routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,15 @@
-const { register, login } = require("../controllers/userController");
+const {
+  register,
+  login,
+  setAvatar,
+  getAllUsers,
+} = require("../controllers/userController");
 
 const router = require("express").Router();
 
 router.post("/Register", register);
 router.post("/Login", login);
+router.post("/setAvatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers);
 
 module.exports = router;
